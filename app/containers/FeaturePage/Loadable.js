055cac7f81cdf5b3221c1f279c44f6bc067eb5a4
Tim Tawan <ttawanc@gmail.com>
2018-04-12 00:32:01 +1000

    changed loadble name

--- app/containers/FeaturePage/Loadable.js
+++ app/containers/FeaturePage/Loadable.js
@@ -6,6 +6,6 @@ import Loadable from 'react-loadable';
 import LoadingIndicator from 'components/LoadingIndicator';
 
 export default Loadable({
-  loader: () => import('./index'),
+  loader: () => require.ensure([], (require) => require("./index"), 'FeaturePage'),
   loading: LoadingIndicator,
 });
