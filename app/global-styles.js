4852d7f792a7d8fc2e8b740bba44d8966ab616e6
Tim Tawan <timtawans@gmail.com>
2018-04-13 00:44:52 +1000

    reset css

--- app/global-styles.js
+++ app/global-styles.js
@@ -4,7 +4,11 @@ import { injectGlobal } from 'styled-components';
 injectGlobal`
   html,
   body {
-    height: 100%;
-    width: 100%;
+    margin: 0;
+    font-family: "Roboto", sans-serif;
+    font-size: 0.9375rem;
+    color: #212529;
+    text-align: left;
+    background-color: #f3f3f3;
   }
 `;
